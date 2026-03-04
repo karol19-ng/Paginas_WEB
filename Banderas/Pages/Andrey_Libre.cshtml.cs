a55279a72c68b9c15276b01f5e8d88e71f28a675
Andrey Calderón Vega <andrey.ac1397@gmail.com>
2026-03-03 20:00:09 -0600

    Proyecto Banderas terminado

---
+++ Banderas/Pages/Andrey_Libre.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Banderas.Pages
+{
+    public class Andrey_LibreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
